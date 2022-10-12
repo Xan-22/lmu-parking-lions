8eafc141a6a4e2985991cf702a13e036079a5530
Xan-22 <83319450+Xan-22@users.noreply.github.com>
2022-10-11 18:58:17 -0700

    added swiftui app repo

---
+++ ParkingLions/ParkingLions/ParkingLionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkingLionsApp.swift
+//  ParkingLions
+//
+//  Created by Xan on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ParkingLionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
